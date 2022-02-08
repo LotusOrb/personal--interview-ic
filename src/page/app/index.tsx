48f477927958bd11c641c20d81a58c751a506e81
LotusOrb <rsyafridamara@gmail.com>
2022-02-09 06:41:45 +0700

    feat : add welcome text

--- src/page/app/index.tsx
+++ src/page/app/index.tsx
@@ -2,5 +2,16 @@ import React from 'react';
 import BaseNavbar from '../../component/BaseNavbar/BaseNavbar';
 
 export default () => {
-  return <BaseNavbar />;
+  return (
+    <React.Fragment>
+      <BaseNavbar />
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <h1>Interview Test</h1>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
 };
